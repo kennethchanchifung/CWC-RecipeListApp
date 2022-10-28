2c012e4e55e8075090f5ea48482656626e6d5fc3
kenneth <che1428@hotmail.com>
2022-10-28 20:48:21 +0800

    - Add RecipeFeatureView - Modify data flow using environment object modifier - Create RecipeModel instance in the RecipeTabView

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by k on 28/10/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView{
+            RecipeFeaturedView()
+                .tabItem{
+                    VStack{
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem{
+                    VStack{
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        .environmentObject(RecipeModel())
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
